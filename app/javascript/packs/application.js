88c9e37db078a2cd34ebd239328dc8ec6e64a99e
hurabittencourt <hura@gama.academy>
2020-07-19 17:51:55 -0300

    Add internationalization, favicon and made some adjusts on layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
+//= require semantic-ui
+//= require jquery
+
+//= require jquery_ujs
+// require turbolinks
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
